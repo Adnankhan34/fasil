11c94bb5a613096267167d44b4487d9883f9c066
ADNAN KHAN <ADNAN KHAN@192.168.10.7>
2016-06-19 11:15:35 -0700

    lfejljlfjsljld

---
+++ src/final1/calculation.java
@@ -0,0 +1,17 @@
+package final1;
+
+public class calculation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("adnan khan day");
+		
+		
+		
+	}
+
+}
